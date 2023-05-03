51019a2b4e22ce4eb2fba753967a0105c38e1383
Vladislav Aituganov <vaaituganov@gmail.com>
2023-05-03 12:34:04 +0300

    Use vite instead of rollup

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,11 @@ import type { App } from 'vue'
 
 import * as components from './components'
 
-const VUIKit = {
-  install(app: App) {
-    // Auto import all components
-    for (const componentKey in components) {
-        app.use((components as any)[componentKey])
-    }
+export function installUiKit(app: App) {
+  // Auto import all components
+  for (const componentKey in components) {
+    console.error('!!!', componentKey, components)
+    app.component(componentKey, (components as any)[componentKey])
   }
 }
-
-export default VUIKit
+export type * from './components'
---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'VUIKit',
+      fileName: 'vui-kit',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        format: 'esm',
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  plugins: [dts(), vue()]
+})
